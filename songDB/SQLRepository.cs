8dccd03aef3fce27da4233350088aa4fde78d172
Hy0tic <vothanhdat278@gmail.com>
2023-11-25 18:24:53 -0500

    add song model, add Sql library

--- songDB/SQLRepository.cs
+++ songDB/SQLRepository.cs
@@ -1,9 +1,49 @@
-﻿namespace songDB;
+﻿ using System.Data.SqlClient;
+
+namespace songDB;
 public class SQLRepository
 {
     private readonly string ConnectionStrings;
     public SQLRepository(string connectionStrings)
     {
         ConnectionStrings = connectionStrings;
+    }
+
+    public void AddSong()
+    {
+        string sqlQuery = "";
+        using (SqlConnection conn = new SqlConnection(ConnectionStrings))
+        {
+            conn.Open();
+
+            SqlCommand command = new SqlCommand(sqlQuery, conn);
+            command.Parameters.AddWithValue("@name", "");
+            command.Parameters.AddWithValue("@album", "");
+
+            command.ExecuteNonQuery();
+        }
+
+        return;
+    }
+
+    public void GetRandomSong()
+    {
+        string sqlQuery = "";
+
+        using (SqlConnection con = new SqlConnection(ConnectionStrings))
+        {
+            con.Open();
+            
+            SqlCommand command = new SqlCommand(sqlQuery, con);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+
+            }
+
+        } 
+
+        return;
     }
 }
---
+++ songDB/Song.cs
@@ -0,0 +1,18 @@
+
+namespace songDB;
+public class Song {
+    public Song(string name, string album, string artist, string lyrics, string mp3)
+    {
+        Name = name;
+        Album = album;
+        Artist = artist;
+        Lyrics = lyrics;
+        Mp3 = mp3;
+    }
+
+    public string Name { get; set; }
+    public string Album { get; set; }
+    public string Artist { get; set;}
+    public string Lyrics { get; set; }
+    public string Mp3 { get; set; }
+}
