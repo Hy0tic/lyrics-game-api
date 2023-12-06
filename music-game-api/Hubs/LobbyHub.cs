f84f53bdbb13abe33be97cf18f4c01486d40b556
Hy0tic <vothanhdat278@gmail.com>
2023-12-05 22:13:30 -0500

    add method to lobby hub

--- music-game-api/Hubs/LobbyHub.cs
+++ music-game-api/Hubs/LobbyHub.cs
@@ -1,8 +1,28 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using music_game_api.Services;
 
 namespace music_game_api.Hubs;
 
 public class LobbyHub : Hub
 {
-    
+    private SongService SongService { get; set; }
+    public LobbyHub(SongService songService)
+    {
+        SongService = songService;
+    }
+
+    public void JoinLobby()
+    {
+        
+    }
+
+    public void GetQuestion()
+    {
+        
+    }
+    public void SendAnswer()
+    {
+        
+    }
+
 }
